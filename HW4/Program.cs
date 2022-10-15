3c0c71b9c23a4a680aaf3b349532175574631213
Дмитро Єремія <diameremia2006@gmail.com>
2022-10-15 13:16:15 +0300

    Add project files.

---
+++ HW4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        int[] a = new int[0];
+        int b = 0;
+        while (b <= 2)
+        {
+            Console.WriteLine("Введіть розмір(більше 2):");
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+        a = new int[b];
+        Console.WriteLine("Введіть перші 2 числа");
+        for (int i = 0; i < 2; i++)
+        {
+            b = Convert.ToInt32(Console.ReadLine());
+            a[i] = b;
+        }
+        for (int i = 2; i < a.Length; i++)
+        {
+            b = a[i - 2] + a[i - 1];
+            a[i] = b;
+        }
+        Console.WriteLine("Результат:   ");
+        foreach (int i in a)
+            Console.WriteLine(i);
+       
+
+    }
+}
+
